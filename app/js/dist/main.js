24a5eee66543b14f57f922dca1bf57c1f1ff40e7
Anton Webb <anton25360@gmail.com>
2019-11-04 23:45:59 +0000

    Fixed drinkInfo bugs. Added new search btn. Added more dividers.

--- app/js/dist/main.js
+++ app/js/dist/main.js
@@ -1,21 +1,74 @@
-function fetchData() {
+setOutputVisibility(false);
+newSearchVisibility(false);
+document.getElementById('searchBtn').addEventListener('click', function fetchData() {
+    setOutputVisibility(true);
+    newSearchVisibility(true);
     var input = document.getElementById('textInput').value;
-    console.log(input);
     fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + input)
         .then(function (data) {
         return data.json();
     })
         .then(function (data) {
         var result = data.drinks[0]; //gets 1st result
-        console.log(result); //logs as object
-        //TODO handlebars
-        // document.querySelector('#target').innerHTML =  '<p>' + result.strDrink + ' is  '+result.strAlcoholic+'</p>'
         document.querySelector('#drinkName').innerHTML = '<p>' + result.strDrink + '</p>';
         document.querySelector('#drinkInfo').innerHTML = '<p>' + result.strAlcoholic + ', ' + result.strCategory + '</p>';
-        // document.querySelector('#drinkType').innerHTML =  '<p>Type: ' + result.strAlcoholic + '</p>'
-        // document.querySelector('#drinkCategory').innerHTML =  '<p>Category: ' + result.strCategory + '</p>'
-        document.querySelector('#drinkInstructions').innerHTML = '<p>Instructions:<br>' + result.strInstructions + '</p>';
+        document.querySelector('#drinkIngredients').innerHTML += '<p> Ingredients:<br></p>';
+        document.querySelector('#drinkInstructions').innerHTML = '<p>Instructions:<br><br>' + result.strInstructions + '</p>';
         document.querySelector('#drinkImg').innerHTML = '<img id="image" src=' + result.strDrinkThumb + ' alt="placeholder">';
-        // document.getElementById("image").style.width = "300px"; 
+        var ingredients = [];
+        var measurements = [];
+        //adds ingredients and their measurements to arrays.
+        Object.keys(result).forEach(function (key) {
+            if (String(key).includes('Ingredient')) {
+                ingredients.push(result[key]);
+                ingredients.forEach(function (element) {
+                    if (element == null) {
+                        ingredients.pop();
+                    }
+                });
+            }
+            if (String(key).includes('Measure')) {
+                measurements.push(result[key]);
+                measurements.forEach(function (element) {
+                    if (element == null) {
+                        measurements.pop();
+                    }
+                });
+            }
+        });
+        var index = 0;
+        ingredients.forEach(function (element) {
+            if (measurements[index] === undefined) {
+                measurements[index] = '';
+            }
+            else {
+                measurements[index] = ' - ' + measurements[index];
+            }
+            document.querySelector('#drinkIngredients').innerHTML += '<p>' + ingredients[index] + measurements[index] + '</p>';
+            index++;
+        });
     });
+});
+function setOutputVisibility(value) {
+    if (value) {
+        document.getElementById('output').style.visibility = 'visible';
+    }
+    else {
+        document.getElementById('output').style.visibility = 'hidden';
+    }
 }
+function newSearchVisibility(value) {
+    if (value) {
+        document.getElementById('textInput').style.visibility = 'hidden';
+        document.getElementById('searchBtn').style.visibility = 'hidden';
+        document.getElementById('searchBtnNew').style.visibility = 'visible';
+    }
+    else {
+        document.getElementById('textInput').style.visibility = 'visible';
+        document.getElementById('searchBtn').style.visibility = 'visible';
+        document.getElementById('searchBtnNew').style.visibility = 'hidden';
+    }
+}
+document.getElementById('searchBtnNew').addEventListener('click', function reloadPage() {
+    location.reload();
+});
--- app/js/main.ts
+++ app/js/main.ts
@@ -1,8 +1,11 @@
+setOutputVisibility(false)
+newSearchVisibility(false)
 
+document.getElementById('searchBtn').addEventListener('click', function fetchData() {
 
-function fetchData() {
+    setOutputVisibility(true)
+    newSearchVisibility(true)
     var input = (<HTMLInputElement>document.getElementById('textInput')).value;
-    console.log(input);
 
     fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + input)
         .then(function (data) {
@@ -10,18 +13,72 @@ function fetchData() {
         })
         .then(function (data) {
             var result = data.drinks[0] //gets 1st result
-            console.log(result); //logs as object
-            
-            //TODO handlebars
-            // document.querySelector('#target').innerHTML =  '<p>' + result.strDrink + ' is  '+result.strAlcoholic+'</p>'
 
-            document.querySelector('#drinkName').innerHTML =  '<p>' + result.strDrink + '</p>'
-            document.querySelector('#drinkInfo').innerHTML =  '<p>' + result.strAlcoholic + ', ' + result.strCategory + '</p>'
-            // document.querySelector('#drinkType').innerHTML =  '<p>Type: ' + result.strAlcoholic + '</p>'
-            // document.querySelector('#drinkCategory').innerHTML =  '<p>Category: ' + result.strCategory + '</p>'
-            document.querySelector('#drinkInstructions').innerHTML =  '<p>Instructions:<br>' + result.strInstructions + '</p>'
-            document.querySelector('#drinkImg').innerHTML = '<img id="image" src='+ result.strDrinkThumb +' alt="placeholder">'
-            // document.getElementById("image").style.width = "300px"; 
+            document.querySelector('#drinkName').innerHTML = '<p>' + result.strDrink + '</p>'
+            document.querySelector('#drinkInfo').innerHTML = '<p>' + result.strAlcoholic + ', ' + result.strCategory + '</p>'
+            document.querySelector('#drinkIngredients').innerHTML += '<p> Ingredients:<br></p>'
+            document.querySelector('#drinkInstructions').innerHTML = '<p>Instructions:<br><br>' + result.strInstructions + '</p>'
+            document.querySelector('#drinkImg').innerHTML = '<img id="image" src=' + result.strDrinkThumb + ' alt="placeholder">'
+
+
+            var ingredients: string[] = []
+            var measurements: string[] = []
+
+            //adds ingredients and their measurements to arrays.
+            Object.keys(result).forEach(key => {
+
+                if (String(key).includes('Ingredient')) {
+                    ingredients.push(result[key])
+
+                    ingredients.forEach(element => {
+                        if (element == null) {
+                            ingredients.pop()
+                        }
+                    });
+                }
+                if (String(key).includes('Measure')) {
+                    measurements.push(result[key])
+
+                    measurements.forEach(element => {
+                        if (element == null) {
+                            measurements.pop()
+                        }
+                    });
+                }
+            });
+
+            var index = 0
+            ingredients.forEach(element => {
+                if (measurements[index] === undefined) {
+                    measurements[index] = ''
+                } else {
+                    measurements[index] = ' - ' + measurements[index]
+                }
+                document.querySelector('#drinkIngredients').innerHTML += '<p>' + ingredients[index] + measurements[index] + '</p>'
+                index++
+            });
         })
+})
+
+function setOutputVisibility(value: boolean){
+    if (value) {
+        document.getElementById('output').style.visibility = 'visible'
+    } else{
+        document.getElementById('output').style.visibility = 'hidden'
+    }
 }
 
+function newSearchVisibility(value: boolean){
+    if (value) {
+        document.getElementById('textInput').style.visibility = 'hidden'
+        document.getElementById('searchBtn').style.visibility = 'hidden'
+        document.getElementById('searchBtnNew').style.visibility = 'visible'
+    } else{
+        document.getElementById('textInput').style.visibility = 'visible'
+        document.getElementById('searchBtn').style.visibility = 'visible'
+        document.getElementById('searchBtnNew').style.visibility = 'hidden'    }
+}
+
+document.getElementById('searchBtnNew').addEventListener('click', function reloadPage() {
+    location.reload(); 
+})
